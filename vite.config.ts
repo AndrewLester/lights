d429b114196da24b98f1f7bd1420b825079fe883
AndrewLester <alester220@gmail.com>
2022-07-20 20:03:43 -0400

    Add baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     build: {
         outDir: "build",
     },
+    base: process.env.VITE_BASE_URL || undefined,
     define: {
         "import.meta.vitest": undefined,
     },
